2f9f82fb60f538554a8ba2b2573e748b876c32e5
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-01-20 16:51:25 +0100

    Modified README. Added respond to sms script

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Twilio sms project
 
 
 1 - Create your virtualenv
-2 - Install twilio ( pip install twilio )
-3 - Set the environment variables TWILIO_ACCOUNT and TWILIO_TOKEN
+
+2 - Install the requirement file ( pip install -r requirements.txt )
+
+3 - Set the environment variables TWILIO_ACCOUNT, TWILIO_TOKEN, TWILIO_NUMBER
 In your local machine, you can just do:
    export TWILIO_ACCOUNT='XXXXX'
    export TWILIO_TOKEN='YYYYY'
-
+   TWILIO_NUMBER='ZZZZZ'
 You can find your Account and Token at http://www.twilio.com/user/account
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 from  twilio.rest import TwilioRestClient
 
+# You can find these tokens at http://www.twilio.com/user/account
 account = os.environ.get('TWILIO_ACCOUNT')
 token = os.environ.get('TWILIO_TOKEN')
+
+# It has to be a Twilio number. It will fail if you do not insert a valid number
+twilio_number = os.environ.get('TWILIO_NUMBER', 'XXXXXXXX')
 
 if not account or not token:
     raise Exception("Account and Token are need for accessing Twilio API")
@@ -11,3 +15,7 @@ client = TwilioRestClient(account, token)
 if __name__ == "__main__":
     if not os.environ.get('TWILIO_ACCOUNT') or not os.environ.get('TWILIO_TOKEN'):
         print "Account and Token are needed for accessing Twilio API"
+    to=raw_input("Which number do you want to send an SMS?")
+    sms_text=raw_input("Write the sms now: ")
+
+    message = client.sms.messages.create(to=to, from_=twilio_number, body=sms_text)
