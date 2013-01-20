38b5e01a0ddff80710ee6e1f90f7cae409ba0698
Domenico Solazzo <solazzo.domenico@gmail.com>
2013-01-20 16:16:36 +0100

    Added exception when the account and token are not available

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import os
+from  twilio.rest import TwilioRestClient
+
 account = os.environ.get('TWILIO_ACCOUNT')
 token = os.environ.get('TWILIO_TOKEN')
 
+if not account or not token:
+    raise Exception("Account and Token are need for accessing Twilio API")
+client = TwilioRestClient(account, token)
 
 if __name__ == "__main__":
     if not os.environ.get('TWILIO_ACCOUNT') or not os.environ.get('TWILIO_TOKEN'):
-        raise Exception("Account and Token are needed for accessing to the Twilio API")
+        print "Account and Token are needed for accessing Twilio API"
